0fd5ed8b66a39eb6970598570d8853bd75c4fa09
Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-11-28 15:33:08 -0300

    feat: Update dependencies and add user validation logic

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"log"
+
+	"github.com/LuizDevExe/go-crud-studies/src/controller/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
 	}
 }
